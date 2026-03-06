13aebf29225b9c7ef7cada7652ff3a48faa5f96f
albionmorina <morinaalbion85@gmail.com>
2026-03-06 11:30:09 +0100

    einfach

---
+++ ErsteVorlesung.py
@@ -0,0 +1,35 @@
+print("2/3 = ", 2/3, "das ist circa", 0.6)
+print("Pi is approximately", 3.14)
+print(3**2) ## das ist wie 3 hoch 2 also "print(3^2)" wird nicht funksionieren für potenzen
+
+print(13//2) # dividirung ohne rest bezeichnet man mit 13//2 = 6   und nicht 6.5
+
+############################################
+
+x= 3.14;
+print("Pi is approximately", x);
+x = 3
+print("Pi is roughly", x);
+y= 2*x + 5;
+print("y=", y)
+
+#variablen nachem bevorzugt (Buchstaben,Zahlen und Untersriche)
+#Die erste Zeichenn darf ken Zahl sein (2pac)
+#variable namen sind immer klein geschrieben und verbunden durch _ z.B first_name oder integral_value
+#Wichtig!!! die Variablen müssen durch namen bezeichnet, die die Variable erklärt
+
+
+################################################
+
+age = input("Enter your age: ") # das nimmt input von Benutzer
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(first_name, last_name, "is", age, "years old.")
+
+x = (2/3)
+
+print("2/3 = {:4.2f}".format(x))
+print("2/3 = {:5.3f}".format(x))
+print("2/3 = {:6.2f}".format(x))
+
+#die zweite methode mit format 
